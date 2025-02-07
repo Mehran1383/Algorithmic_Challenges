56baf68dbffae5d57ac26e5d1c7318113783a68b
Mehran <mehranstock1383@gmail.com>
2025-02-07 12:38:09 +0330

    Initial commit

---
+++ Quera/چاپ لوزی.cpp
@@ -0,0 +1,19 @@
+﻿https://quera.org/problemset/618
+
+#include <iostream>
+using namespace std;
+
+int main() {
+	int n;
+	cin >> n;
+	for (int j = 0; j < 2 * n + 1; j++) {
+		for (int i = 0; i < 2 * n + 1; i++) {
+			if (i >= n - j && i <= n + j && j <= n) cout << "*";
+			else if (i >= abs(n - j) && i <= 3 * n - j && j>n) cout << "*";
+			else cout << " ";
+		}
+		cout << endl;
+	}
+
+
+}
